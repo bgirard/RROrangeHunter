ce4fc566fbc34cc0414ca91170c5d7470a1647e7
Benoit Girard <b56girard@gmail.com>
2015-07-23 14:56:01 -0400

    Add depends for gtk3

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ set -e
 
 # system setup, run once
 sudo apt-get update
-sudo apt-get install -qq gdb python-pip Xvfb unzip firefox mailutils
+sudo apt-get install -qq gdb python-pip Xvfb unzip firefox mailutils libgtk-3-dev libcairo2-dev
 if [ ! -e "rr-3.2.0-Linux-$(uname -m).deb" ]; then
   wget https://mozilla.github.io/rr/releases/rr-3.2.0-Linux-$(uname -m).deb
 fi
